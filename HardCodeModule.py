1a7ae289a305eff9ef1c0a3e1d9713b7c10b1ce4
Jesper <jesper.vanbondt@icloud.com>
2018-04-01 23:54:22 +0200

    2 Commands in hardcode module
    Added 2 new fun things in the hardcode module
    Note: These are personal to my life

--- HardCodeModule.py
+++ HardCodeModule.py
@@ -2,8 +2,12 @@ class HardCoded:
     def Questions(text):
         if text == "how are you":
             return "I'm fine thanks"
-        elif text == "hey jarvis fjf":
-            return "fuh"
+        elif text == "who is jef":
+            return "he is the one that is in love with a 13 year old"
+        elif text == "am i being haunted"
+            return "your tech ghost is right behind you"
+        elif text == "why isn't my internet working"
+            return "because you live in belgium and you have telenet as isp"
         else:
             return False
         
